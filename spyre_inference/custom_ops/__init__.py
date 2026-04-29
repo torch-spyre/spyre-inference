934a643abfe05242f5808d24b4d877c1baa62f8b
Thomas Ortner <boh@zurich.ibm.com>
2026-04-29 10:23:16 +0000

    Integrated rms_norm vLLM IR provider
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- spyre_inference/custom_ops/__init__.py
+++ spyre_inference/custom_ops/__init__.py
@@ -3,7 +3,6 @@
 from functools import lru_cache
 
 from . import parallel_lm_head
-from . import rms_norm
 from . import rotary_embedding
 from . import silu_and_mul
 from . import vocab_parallel_embedding
@@ -19,9 +18,9 @@ def register_all():
     vocab_parallel_embedding.register()
     parallel_lm_head.register()
     rotary_embedding.register()
-    rms_norm.register()
     silu_and_mul.register()
     linear.register()
 
     # IR provider registration (triggered by import)
+    logger.info("Registering vLLM IR providers for spyre_inference")
     from . import kernels as _kernels  # noqa: F401

55bac45ee8e510f30040e2d9675e9fc9b249d9c1
Prashant Gupta <prashantgupta@us.ibm.com>
2026-08-14 19:10:29 +0000

    refactor: remove gelu_and_mul and silu_and_mul imports
    Update custom_ops __init__.py to remove imports for the deleted
    wrapper modules.
    
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- spyre_inference/custom_ops/__init__.py
+++ spyre_inference/custom_ops/__init__.py
@@ -16,14 +16,12 @@
 
 from functools import lru_cache
 
-from . import gelu_and_mul  # noqa: F401
 from . import gemma_rms_norm  # noqa: F401
 from . import logits_processor  # noqa: F401
 from . import parallel_lm_head
 from . import rms_norm
 from . import rotary_embedding
 from . import linear
-from . import silu_and_mul
 from . import utils
 from . import vocab_parallel_embedding  # noqa: F401
 from vllm.logger import init_logger

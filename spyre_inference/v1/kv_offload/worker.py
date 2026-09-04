0f57ba3a73b2fee62f2f1c93a34c23f02ec69384
Timothy Hakobian <thakobian@ibm.com>
2026-09-04 14:52:56 -0400

    Add calls SpyreKvDmaCopier to do the device <-> host copies. However, kv cache pages indexing from these funcs needs to be changed
    Signed-off-by: Timothy Hakobian <thakobian@ibm.com>

--- spyre_inference/v1/kv_offload/worker.py
+++ spyre_inference/v1/kv_offload/worker.py
@@ -21,6 +21,8 @@ from vllm.v1.kv_offload.base import (
     TransferResult,
 )
 
+from spyre_inference.v1.kv_offload.copier import SpyreKvDmaCopier
+
 # M1-S1: In progress on other branch:
 
 
@@ -28,26 +30,46 @@ logger = init_logger(__name__)
 
 
 class SpyreOffloadingWorker(OffloadingWorker):
-    def submit_store(job_id, src_spec: GPULoadStoreSpec, dst_spec: LoadStoreSpec) -> bool:
+    def __init__(self, kv_caches, pool):
+        self._finished_jobs: list[TransferResult] = []
+        self._kv_caches = kv_caches
+        self._pool = pool
+        self._copier = SpyreKvDmaCopier()
+
+    def submit_store(
+        self, job_id: int, src_spec: GPULoadStoreSpec, dst_spec: LoadStoreSpec
+    ) -> bool:
         """
         Start an async copy for device to host.
         """
-        pass
+        for device_blk_id, slot_id in zip(src_spec.block_ids, dst_spec.block_ids):
+            self._copier.copy_d2h(self._kv_caches[device_blk_id], self._pool, slot_id)
 
-    def submit_load(job_id, src_spec: LoadStoreSpec, dst_spec: GPULoadStoreSpec) -> bool:
+        self._finished_jobs.append(TransferResult(job_id=job_id, success=True))
+        return True
+
+    def submit_load(self, job_id: int, src_spec: LoadStoreSpec, dst_spec: GPULoadStoreSpec) -> bool:
         """
         Start an async copy for host to device.
         """
-        pass
+        for slot_id, device_blk_id in zip(src_spec.block_ids, dst_spec.block_ids):
+            self._copier.copy_h2d(self._kv_caches[device_blk_id], self._pool, slot_id)
+
+        self._finished_jobs.append(TransferResult(job_id=job_id, success=True))
+        return True
 
     def get_finished(self) -> list[TransferResult]:
         """
         Returns all the TransferResults the jobs that are completed.
         """
-        pass
+        finished_jobs = self._finished_jobs
+        self._finished_jobs = []
+        return finished_jobs
 
-    def wait(job_ids: set[int]) -> None:
+    def wait(self, job_ids: set[int]) -> None:
         """
         Block until those specific job ids are done.
         """
+        # In this implementation, we assume that all jobs are completed immediately
+        # after submission, so we don't need to do any waiting.
         pass
